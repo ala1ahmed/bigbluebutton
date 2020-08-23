de4a6f07fcfffa4427956d61c03e50739b798a07
ala1ahmed <38598250+ala1ahmed@users.noreply.github.com>
2020-08-23 13:06:31 +0100

    Update component.jsx

--- bigbluebutton-html5/imports/ui/components/actions-bar/desktop-share/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/desktop-share/component.jsx
@@ -166,7 +166,7 @@ const DesktopShare = ({
         icon={isVideoBroadcasting ? 'desktop' : 'desktop_off'}
         label={intl.formatMessage(vLabel)}
         description={intl.formatMessage(vDescr)}
-        color={isVideoBroadcasting ? 'primary' : 'danger'}
+        color={isVideoBroadcasting ? 'primary' : 'primary'}
         hideLabel
         circle
         size="lg"
