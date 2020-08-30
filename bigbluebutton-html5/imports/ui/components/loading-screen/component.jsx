01aa3edc576eca0f80b3fffea465a21f6aa3b827
ala1ahmed <38598250+ala1ahmed@users.noreply.github.com>
2020-08-30 08:16:56 +0100

    update loading

--- bigbluebutton-html5/imports/ui/components/loading-screen/component.jsx
+++ bigbluebutton-html5/imports/ui/components/loading-screen/component.jsx
@@ -3,11 +3,14 @@ import './animate.css';
 import './styles.css';
 
 const LoadingScreen = ({ children }) => (
-  <div className="arrow-container animated fadeInDown">
+  <div className="container">
+    <div className="arrow-container animated fadeInDown">
     <div className="arrow-2">
       <i className="icon-taderok" ></i>
     </div>
     <div className="arrow-1 animated hinge forever zoomIn"></div>
+  </div>
+
   </div>
 );
 
